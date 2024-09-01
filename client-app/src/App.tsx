7847a4f85feeb745e3718f9e3b25220234bd58b8
Kuba Kubiszyn <kubakubiszyn97@gmail.com>
2024-09-01 21:56:20 +0200

    react - walking skeleton

---
+++ client-app/src/App.tsx
@@ -0,0 +1,29 @@
+import { act, useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+  const [acitivites, setActivites] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivites(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+        {acitivites.map((activity : any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  )
+}
+
+export default App
