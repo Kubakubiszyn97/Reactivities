3e569a5c4cfa056954962b46bcceacc829c6dee5
Kuba Kubiszyn <kubakubiszyn97@gmail.com>
2024-09-29 21:26:28 +0200

    add Activity Users relations part 1

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -21,7 +21,12 @@ namespace Application.Activities
 
             public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken token)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(token));
+                var activities = await _context.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(token);
+                
+                return Result<List<Activity>>.Success(activities);
             }
         }
     }
